6be42bbb662773b6f5a3d9b1f5d1cc4c44e45b7a
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-19 23:25:44 +0000

    ADD navBar textStyle

--- lib/style/styles.dart
+++ lib/style/styles.dart
@@ -1,1 +1,9 @@
-class Styles {}
+import 'package:flutter/material.dart';
+
+class Styles {
+  static final TextStyle navBarElement = TextStyle(
+      fontWeight: FontWeight.normal,
+      fontFamily: 'Nunito',
+      fontSize: 18,
+      color: Colors.black);
+}
