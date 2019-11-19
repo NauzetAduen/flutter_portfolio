af3e99e52c18f228e698b6f8402a635adc2f8650
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-19 23:43:49 +0000

    ADD top navbar

---
+++ lib/widgets/customTopNavBar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/style/styles.dart';
+import 'package:flutter_portfolio/widgets/nav_bar_element.dart';
+
+class CustomTopNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'nau.',
+          style: Styles.nauTextStyle,
+        ),
+        Row(
+          children: <Widget>[
+            NavBarElement('Info'),
+            NavBarElement('Experience'),
+            NavBarElement('Projects'),
+            NavBarElement('Skills and interests'),
+            NavBarElement('Contact'),
+          ],
+        )
+      ],
+    );
+  }
+}
