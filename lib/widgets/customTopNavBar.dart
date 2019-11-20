39ac40267836719696de8da177ec0915c2bd4c5b
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-20 03:09:52 +0000

    REMOVE example colors and change info for home

--- lib/widgets/customTopNavBar.dart
+++ lib/widgets/customTopNavBar.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/style/styles.dart';
-import 'package:flutter_portfolio/widgets/nav_bar_element.dart';
+import 'package:flutter_portfolio/widgets/navBarElement.dart';
 
 class CustomTopNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100,
-      color: Colors.red,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
@@ -17,7 +16,9 @@ class CustomTopNavBar extends StatelessWidget {
           ),
           Row(
             children: <Widget>[
-              NavBarElement('Info'),
+              NavBarElement(
+                'Home',
+              ),
               NavBarElement('Experience'),
               NavBarElement('Projects'),
               NavBarElement('Skills and interests'),
