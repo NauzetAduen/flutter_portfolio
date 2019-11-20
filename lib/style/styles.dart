6632bbe48082567eae81352bc5ffb34b867a41ce
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-20 03:09:22 +0000

    ADD welcome text style and navbar bold

--- lib/style/styles.dart
+++ lib/style/styles.dart
@@ -6,9 +6,21 @@ class Styles {
       fontFamily: 'Nunito',
       fontSize: 18,
       color: Colors.black);
+  static final TextStyle navBarElementBold = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Nunito',
+      fontSize: 18,
+      color: Colors.black);
   static final TextStyle nauTextStyle = TextStyle(
       fontWeight: FontWeight.bold,
       fontFamily: 'Nunito',
-      fontSize: 48,
+      fontSize: 56,
+      color: Colors.black);
+  static final TextStyle welcomeText = TextStyle(
+      letterSpacing: 2,
+      height: 2,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Nunito',
+      fontSize: 32,
       color: Colors.black);
 }
