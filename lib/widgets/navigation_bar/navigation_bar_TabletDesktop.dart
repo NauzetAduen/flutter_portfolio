2884b10da5a9612a4235a0394640baee6d69a871
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-26 14:01:53 +0000

    FIX unnused imports

--- lib/widgets/navigation_bar/navigation_bar_TabletDesktop.dart
+++ lib/widgets/navigation_bar/navigation_bar_TabletDesktop.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_portfolio/style/styles.dart';
 import 'package:flutter_portfolio/widgets/navbar_item.dart';
 import 'package:flutter_portfolio/widgets/navigation_bar/navbar_logo.dart';
 
