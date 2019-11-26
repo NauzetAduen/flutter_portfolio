1db77edc266c22fe67b201d386b3bbb9150e1954
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-26 13:25:48 +0000

    REMOVE unnecesary Container

--- lib/widgets/navigation_bar/navigation_bar_TabletDesktop.dart
+++ lib/widgets/navigation_bar/navigation_bar_TabletDesktop.dart
@@ -7,20 +7,18 @@ import 'package:flutter_portfolio/widgets/navigation_bar/navbar_logo.dart';
 class NavigationBarTabletDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          NavBarLogo(),
-          Wrap(
-            children: <Widget>[
-              NavBarElement('Me'),
-              NavBarElement('Experience'),
-              NavBarElement('Projects'),
-            ],
-          )
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        NavBarLogo(),
+        Wrap(
+          children: <Widget>[
+            NavBarElement('Me'),
+            NavBarElement('Experience'),
+            NavBarElement('Projects'),
+          ],
+        )
+      ],
     );
   }
 }
