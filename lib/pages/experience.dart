f234b3a148a7031cca6c84ecf80fc4c787086a30
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-27 09:33:57 +0000

    ADD simple experience view to work with

---
+++ lib/pages/experience.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Experience extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("P"),
+      ),
+    );
+  }
+}
