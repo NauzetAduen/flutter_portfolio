08c7344a34b52f385d7d2833391ab34db11048d0
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-27 09:35:03 +0000

    REWORK navbar item to use navigator correctly

--- lib/widgets/navigation_bar/navbar_item.dart
+++ lib/widgets/navigation_bar/navbar_item.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/style/styles.dart';
 
-class NavBarElement extends StatelessWidget {
+class NavBarItem extends StatelessWidget {
   final String text;
-
-  const NavBarElement(this.text);
+  const NavBarItem({Key key, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        style: Styles.navBarElementBold,
+      child: GestureDetector(
+        onTap: () => Navigator.pushNamed(context, "/$text"),
+        child: Text(
+          '$text',
+          style: Styles.navBarElementBold,
+        ),
       ),
     );
   }
