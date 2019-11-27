79f412d00053633d4586ab2edd9ed29e203ffe6d
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-27 12:51:12 +0000

    ADD info to experience page

--- lib/pages/experience.dart
+++ lib/pages/experience.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/style/styles.dart';
+import 'package:flutter_portfolio/utils/experience_item_list.dart';
+import 'package:flutter_portfolio/widgets/centered_widget.dart';
+import 'package:flutter_portfolio/widgets/navigation_bar/navigation_bar.dart';
 
 class Experience extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Experience"),
-      ),
+    return CenteredWidget(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Column(
+            children: <Widget>[
+              NavigationBar(),
+              Expanded(
+                child: ListView(
+                  children: ExperienceItemList.list,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
