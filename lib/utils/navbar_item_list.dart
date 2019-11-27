418da9d19300ae18b9a51536d08085d6b7d97185
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-27 08:55:36 +0000

    SEPARATE nabbar items into a static list

---
+++ lib/utils/navbar_item_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/widgets/navigation_bar/navbar_item.dart';
+
+class NavBarItemList {
+  static List<Widget> list = [
+    NavBarElement('Me'),
+    NavBarElement('Experience'),
+    NavBarElement('Projects'),
+  ];
+}
--- lib/widgets/navigation_bar/navigation_bar_TabletDesktop.dart
+++ lib/widgets/navigation_bar/navigation_bar_TabletDesktop.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_portfolio/widgets/navigation_bar/navbar_item.dart';
+import 'package:flutter_portfolio/utils/navbar_item_list.dart';
 import 'package:flutter_portfolio/widgets/navigation_bar/navbar_logo.dart';
 
 class NavigationBarTabletDesktop extends StatelessWidget {
@@ -10,13 +10,7 @@ class NavigationBarTabletDesktop extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         NavBarLogo(),
-        Wrap(
-          children: <Widget>[
-            NavBarElement('Me'),
-            NavBarElement('Experience'),
-            NavBarElement('Projects'),
-          ],
-        )
+        Wrap(children: NavBarItemList.list),
       ],
     );
   }
