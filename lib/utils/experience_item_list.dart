a394709910dfda3788faa06bb6633adb3f6aa36f
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-11-30 15:08:37 +0000

    ADD linkMessage to each item

--- lib/utils/experience_item_list.dart
+++ lib/utils/experience_item_list.dart
@@ -10,6 +10,7 @@ class ExperienceItemList {
           "Collaboration scholarship at ULPGC’s Vice-chancellor for students and employability",
       desc:
           "Webs maintenance with Joomla and vanilla PHP\nMySQL Databases maintenance\nWebsite development for university event with Wordpress\n",
+      linkMessage: "16 jornadas Sue - Closed",
       url: "http://16jornadasdesue.ulpgc.es/",
       chips: [
         Chip(
@@ -55,6 +56,7 @@ class ExperienceItemList {
       desc:
           "Improve and increase test coverage in a mobile app\nTested app’s CORE and API REST behind it\nGit, Testing, RESTful web services, Mobile development, Jenkins, SonarQube, Tomcat, DBeaver, ARC, …",
       url: "https://play.google.com/store/apps/details?id=com.inerza.miplaza",
+      linkMessage: "PlayStore - miPlaza",
       chips: [
         Chip(
           backgroundColor: Colors.yellow,
@@ -85,6 +87,7 @@ class ExperienceItemList {
       desc:
           "Developed a “Mobile app and backend to manage hiking trails”\nLearned Flutter framework, Dart Programming Language and cloud firestore ecosystem\nGoogle+ Auth, Firestore Auth, Google Maps, Json serialization and NoSQL, Reactive Programming, …",
       url: "https://github.com/NauzetAduen/HikingApp",
+      linkMessage: "HikingApp - Github (Private)",
       chips: [
         Chip(
           backgroundColor: Colors.red,
@@ -128,6 +131,7 @@ class ExperienceItemList {
       title: "Personal project GroceryListApp",
       desc: "Developed a personal Project Grocery list app with Flutter",
       url: "https://github.com/NauzetAduen/grocery_list_app",
+      linkMessage: "GroceryList App - Github",
       chips: [
         Chip(
           backgroundColor: Colors.indigo,
@@ -158,6 +162,7 @@ class ExperienceItemList {
       desc:
           "A small project to create my portfolio in Flutter and learn Flutter web.",
       url: "https://github.com/NauzetAduen/flutter_portfolio",
+      linkMessage: "PortFolio - Github",
       chips: [
         Chip(
           backgroundColor: Color(0xff1389FD),
