d369acfd3be8a033e12de4a5166a1c48fd7e5d9c
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-12-02 17:48:50 +0000

    FIX a bug in firefox with new lines

--- lib/utils/about_item_list.dart
+++ lib/utils/about_item_list.dart
@@ -6,27 +6,27 @@ class AboutItemList {
     AboutItem(
       title: "Education 2013-2019",
       info:
-          "Computer Engineer at University of Las Palmas de Gran Canaria, Spain\nSoftware Engineer intensification",
+          "Computer Engineer at University of Las Palmas de Gran Canaria, Spain \nSoftware Engineer intensification",
     ),
     AboutItem(
       title: "Technical skills",
       info:
-          "Crossplatform development experience with Flutter\nBasic Android native development\nTesting, Git, differents backends (nosql, api rest)",
+          "Crossplatform development experience with Flutter \nBasic Android native development\nTesting, Git, differents backends (nosql, api rest)",
     ),
     AboutItem(
       title: "Personal skills",
       info:
-          "Communicative, passionate, reliable\nAble to work on my own (Remote)\nEaget to learn new things",
+          "Communicative, passionate, reliable\nAble to work on my own (Remote) \nEaget to learn new things",
     ),
     AboutItem(
       title: "Interests",
       info:
-          "Flutter and mobile development\nClean Architecture, TDD\nImprove programming skills",
+          "Flutter and mobile development\nClean Architecture, TDD \nImprove programming skills",
     ),
     AboutItem(
       title: "Objetives",
       info:
-          "Junior position in Flutter or mobile development\nRemote if possible",
+          "Junior position in Flutter or mobile development \nRemote if possible",
     ),
   ];
 }
