723575ac56fd200cec732721101464342a3b12eb
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-12-02 11:41:37 +0000

    ADD scaffolds into remaining pages

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/widgets/centered_widget.dart';
+import 'package:flutter_portfolio/widgets/navigation_bar/navigation_bar.dart';
 
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("About"),
-      ),
+    return CenteredWidget(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Column(
+            children: <Widget>[
+              NavigationBar(),
+              Expanded(
+                  child: Center(
+                child: Text("About"),
+              ))
+            ],
+          )),
     );
   }
 }
--- lib/pages/error_page.dart
+++ lib/pages/error_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/widgets/centered_widget.dart';
+import 'package:flutter_portfolio/widgets/navigation_bar/navigation_bar.dart';
 
 class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text("Page not found 404"),
-    ));
+    return CenteredWidget(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Column(
+            children: <Widget>[
+              NavigationBar(),
+              Expanded(
+                  child: Center(
+                child: Text("Error"),
+              ))
+            ],
+          )),
+    );
   }
 }
