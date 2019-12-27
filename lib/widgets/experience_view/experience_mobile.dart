578db21599b8a8d0ab8321373a3c60d99675c9ad
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-12-27 15:33:34 +0000

    ADD divider in experience page on mobile

--- lib/widgets/experience_view/experience_mobile.dart
+++ lib/widgets/experience_view/experience_mobile.dart
@@ -4,8 +4,25 @@ import 'package:flutter_portfolio/utils/experience_item_list.dart';
 class ExperienceMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: ExperienceItemList.list,
+    int listSize = ExperienceItemList.list.length;
+    return ListView.builder(
+      itemCount: listSize,
+      itemBuilder: (context, index) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ExperienceItemList.list[index],
+            listSize - 1 != index
+                ? Divider(
+                    indent: 50.0,
+                    endIndent: 50.0,
+                    thickness: 1.0,
+                    color: Colors.black,
+                  )
+                : SizedBox()
+          ],
+        );
+      },
     );
   }
 }
