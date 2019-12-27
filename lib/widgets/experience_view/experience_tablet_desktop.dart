a8e22991a7bd6a123dbb659ae23733b564538130
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-12-27 15:30:29 +0000

    ADD divider after experienceItems

--- lib/widgets/experience_view/experience_tablet_desktop.dart
+++ lib/widgets/experience_view/experience_tablet_desktop.dart
@@ -5,15 +5,39 @@ import 'package:flutter_portfolio/widgets/navigation_bar/navigation_bar.dart';
 class ExperienceTabletDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int listSize = ExperienceItemList.list.length;
     return Column(
       children: <Widget>[
         NavigationBar(),
         Expanded(
-          child: ListView(
-            children: ExperienceItemList.list,
+          // child: ListView(
+          //   children: ExperienceItemList.list,
+          // ),
+          child: ListView.builder(
+            itemCount: listSize,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ExperienceItemList.list[index],
+                  _isLastElement(listSize, index)
+                      ? Divider(
+                          indent: 145.0,
+                          endIndent: 145.0,
+                          thickness: 3.0,
+                          color: Colors.black,
+                        )
+                      : SizedBox()
+                ],
+              );
+            },
           ),
         )
       ],
     );
+  }
+
+  bool _isLastElement(int listSize, int index) {
+    return listSize - 1 != index;
   }
 }
