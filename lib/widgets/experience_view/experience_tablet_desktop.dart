15dda23459b1de6e3cab1df55afdb49320f14208
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-01-12 01:06:08 +0000

    ADD route for detailed

--- lib/widgets/experience_view/experience_tablet_desktop.dart
+++ lib/widgets/experience_view/experience_tablet_desktop.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/utils/experience_item_list.dart';
 import 'package:flutter_portfolio/widgets/navigation_bar/navigation_bar.dart';
 
+import 'experience_item.dart';
+
 class ExperienceTabletDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,20 +15,41 @@ class ExperienceTabletDesktop extends StatelessWidget {
           child: ListView.builder(
             itemCount: listSize,
             itemBuilder: (context, index) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ExperienceItemList.list[index],
-                  listSize - 1 != index
-                      ? Divider(
-                          indent: 145.0,
-                          endIndent: 145.0,
-                          thickness: 3.0,
-                          color: Colors.black,
-                        )
-                      : SizedBox()
-                ],
+              ExperienceItem expItem = ExperienceItemList.list[index];
+
+              return IconButton(
+                icon: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Icon(Icons.keyboard_arrow_right, size: 45),
+                    Text(expItem.title),
+                  ],
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/detailed", arguments: index);
+                },
               );
+              // return Padding(
+              //     padding: const EdgeInsets.symmetric(vertical: 20),
+              //     child: Text(expItem.title));
+              //   return Column(
+              //     mainAxisSize: MainAxisSize.min,
+              //     children: <Widget>[
+              //       Container(
+              //         height: 15,
+              //         color: Colors.red,
+              //       ),
+              //       // ExperienceItemList.list[index],
+              //       listSize - 1 != index
+              //           ? Divider(
+              //               indent: 145.0,
+              //               endIndent: 145.0,
+              //               thickness: 3.0,
+              //               color: Colors.black,
+              //             )
+              //           : SizedBox()
+              //     ],
+              //   );
             },
           ),
         )
