84ebaf620239f840004f5cde8f3f97f6f2a4e8d4
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-01-12 03:50:24 +0000

    FIX bug with small height

--- lib/widgets/experience_view/experience_tablet_desktop.dart
+++ lib/widgets/experience_view/experience_tablet_desktop.dart
@@ -10,53 +10,55 @@ class ExperienceTabletDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int listSize = ExperienceItemList.list.length;
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        NavigationBar(),
-        Padding(
-          padding: const EdgeInsets.only(top: 40),
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: listSize,
-            itemBuilder: (context, index) {
-              ExperienceItem expItem = ExperienceItemList.list[index];
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          NavigationBar(),
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: listSize,
+              itemBuilder: (context, index) {
+                ExperienceItem expItem = ExperienceItemList.list[index];
 
-              return IconButton(
-                alignment: Alignment.centerLeft,
-                icon: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.keyboard_arrow_right,
-                      size: 35,
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Text(
-                        expItem.title,
-                        style: Styles.experienceListitem,
-                        overflow: TextOverflow.ellipsis,
+                return IconButton(
+                  alignment: Alignment.centerLeft,
+                  icon: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.keyboard_arrow_right,
+                        size: 35,
                       ),
-                    ),
-                  ],
-                ),
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                            title: Text(expItem.title),
-                            content: expItem.toDesktop());
-                      });
-                },
-              );
-            },
-          ),
-        )
-      ],
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          expItem.title,
+                          style: Styles.experienceListitem,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                              title: Text(expItem.title),
+                              content: expItem.toDesktop());
+                        });
+                  },
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
