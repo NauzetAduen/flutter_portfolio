7dc6e5cec7aa16c15a6d012807137b68268692a3
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-01-12 02:11:25 +0000

    REMOVE statelesswidget from experienceitem

--- lib/widgets/experience_view/experience_item.dart
+++ lib/widgets/experience_view/experience_item.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/widgets/experience_view/exp_item_mobile.dart';
 import 'package:flutter_portfolio/widgets/experience_view/exp_item_tablet_desktop.dart';
 
-import 'package:responsive_builder/responsive_builder.dart';
-
-class ExperienceItem extends StatelessWidget {
+class ExperienceItem {
   final String date;
   final String title;
   final String desc;
@@ -22,27 +20,27 @@ class ExperienceItem extends StatelessWidget {
       this.chips,
       this.image});
 
-  @override
-  Widget build(BuildContext context) {
-    return ScreenTypeLayout(
-      mobile: ExperienceItemMobile(
-        date: date,
-        title: title,
-        desc: desc,
-        url: url,
-        linkMessage: linkMessage,
-        chips: chips,
-        image: image,
-      ),
-      tablet: ExperienceItemTabletDesktop(
-        date: date,
-        title: title,
-        desc: desc,
-        url: url,
-        linkMessage: linkMessage,
-        chips: chips,
-        image: image,
-      ),
+  ExperienceItemTabletDesktop toDesktop() {
+    return ExperienceItemTabletDesktop(
+      date: date,
+      title: title,
+      desc: desc,
+      url: url,
+      linkMessage: linkMessage,
+      chips: chips,
+      image: image,
+    );
+  }
+
+  ExperienceItemMobile toMobile() {
+    return ExperienceItemMobile(
+      date: date,
+      title: title,
+      desc: desc,
+      url: url,
+      linkMessage: linkMessage,
+      chips: chips,
+      image: image,
     );
   }
 }
