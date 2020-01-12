08d4dab58c61a232fd5f2ea5fde7fd6ae2b95ace
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-01-12 03:20:57 +0000

    REMOVE detailed route

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -3,7 +3,6 @@ import 'package:flutter_portfolio/pages/about.dart';
 import 'package:flutter_portfolio/pages/error_page.dart';
 import 'package:flutter_portfolio/pages/experience.dart';
 import 'package:flutter_portfolio/pages/homepage.dart';
-import 'package:flutter_portfolio/widgets/experience_view/detailed_experience_dialog.dart';
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -14,13 +13,6 @@ class Router {
         return MaterialPageRoute(builder: (_) => Experience());
       case '/about':
         return MaterialPageRoute(builder: (_) => About());
-      case '/detailed':
-        final args = settings.arguments;
-        if (args is int) {
-          return MaterialPageRoute(
-              builder: (_) => DetailedExperience(args), fullscreenDialog: true);
-        }
-        return MaterialPageRoute(builder: (_) => ErrorPage());
       default:
         return MaterialPageRoute(builder: (_) => ErrorPage());
     }
