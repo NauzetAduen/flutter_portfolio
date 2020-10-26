80a8427680cff7a40a24829a59db98e844588a58
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-10-26 15:36:25 +0000

    ADD about view with timeline

---
+++ lib/widgets/about_view/about_view.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:timeline_tile/timeline_tile.dart';
+
+import '../../utils/about_item_list.dart';
+import '../navigation_bar/navigation_bar.dart';
+
+final double indicatorSize = 25.0;
+final LineStyle lineStyle = LineStyle(color: Colors.lightBlue, thickness: 4);
+final EdgeInsets indicatorPadding = EdgeInsets.symmetric(vertical: 12);
+
+class AboutView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NavigationBar(),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: AboutItemList.list.length,
+            itemBuilder: (context, index) {
+              var item = AboutItemList.list[index];
+              if (index == 0)
+                return AboutTimeLine(item, isFirst: true, isLast: false);
+              if (index == AboutItemList.list.length - 1)
+                return AboutTimeLine(item, isFirst: false, isLast: true);
+              return AboutTimeLine(item, isFirst: false, isLast: false);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AboutTimeLine extends StatelessWidget {
+  final AboutItem aboutItem;
+  final bool isFirst;
+  final bool isLast;
+
+  const AboutTimeLine(this.aboutItem, {this.isFirst, this.isLast});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: TimelineTile(
+        isFirst: isFirst,
+        isLast: isLast,
+        afterLineStyle: lineStyle,
+        beforeLineStyle: lineStyle,
+        lineXY: 0.3,
+        alignment: TimelineAlign.manual,
+        indicatorStyle: IndicatorStyle(
+          padding: indicatorPadding,
+          indicatorXY: 0.5,
+          width: indicatorSize,
+          height: indicatorSize,
+          color: Colors.deepOrange,
+        ),
+        startChild: Padding(
+          padding: const EdgeInsets.only(right: 35),
+          child: Text(
+            aboutItem.title,
+            style: Theme.of(context).textTheme.headline6,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+          ),
+        ),
+        endChild: Padding(
+          padding: const EdgeInsets.only(left: 35),
+          child: ResponsiveBuilder(
+            builder: (context, info) => Text(aboutItem.info,
+                style: info.deviceScreenType == DeviceScreenType.mobile
+                    ? Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(fontSize: 14)
+                    : Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(fontSize: 22)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutItem {
+  final String title;
+  final String info;
+
+  const AboutItem({this.title, this.info});
+}
