4f9c49a747433da5aa18a34ee802749304032acd
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-27 22:46:10 +0000

    REMOVE everything

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_portfolio/utils/router.dart' as router;
-import 'package:provider/provider.dart';
-import 'package:flutter_portfolio/style/theme.dart' as theme;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    router.RouteName routeName = router.RouteName("home");
-    return ChangeNotifierProvider.value(
-      value: routeName,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Nauzet, Portfolio',
-        onGenerateRoute: router.Router.generateRoute,
-        initialRoute: "/",
-        theme: theme.themeData,
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nauzet, Portfolio',
+      initialRoute: "/",
+      home: Text("Hi"),
     );
   }
 }
--- lib/utils/about_item_list.dart
+++ lib/utils/about_item_list.dart
@@ -1,31 +1,31 @@
-import '../widgets/about_view/about_view.dart';
+// import '../widgets/about_view/about_view.dart';
 
-class AboutItemList {
-  static List<AboutItem> list = [
-    AboutItem(
-      title: "Education",
-      info:
-          "Computer Engineer at University of Las Palmas de Gran Canaria, Spain\nSoftware Engineer intensification",
-    ),
-    AboutItem(
-      title: "Tech",
-      info:
-          "Crossplatform development experience with Flutter\nBasic Android native development\nTesting, Git, differents backends (nosql, api rest)",
-    ),
-    AboutItem(
-      title: "Soft",
-      info:
-          "Communicative, passionate, reliable\nAble to work on my own (Remote)\nEager to learn new things",
-    ),
-    AboutItem(
-      title: "Interests",
-      info:
-          "Flutter and mobile development\nClean Architecture, TDD\nImprove programming skills",
-    ),
-    AboutItem(
-      title: "Objetives",
-      info:
-          "Junior position in Flutter or mobile development\nRemote if possible",
-    ),
-  ];
-}
+// class AboutItemList {
+//   static List<AboutItem> list = [
+//     AboutItem(
+//       title: "Education",
+//       info:
+//           "Computer Engineer at University of Las Palmas de Gran Canaria, Spain\nSoftware Engineer intensification",
+//     ),
+//     AboutItem(
+//       title: "Tech",
+//       info:
+//           "Crossplatform development experience with Flutter\nBasic Android native development\nTesting, Git, differents backends (nosql, api rest)",
+//     ),
+//     AboutItem(
+//       title: "Soft",
+//       info:
+//           "Communicative, passionate, reliable\nAble to work on my own (Remote)\nEager to learn new things",
+//     ),
+//     AboutItem(
+//       title: "Interests",
+//       info:
+//           "Flutter and mobile development\nClean Architecture, TDD\nImprove programming skills",
+//     ),
+//     AboutItem(
+//       title: "Objetives",
+//       info:
+//           "Junior position in Flutter or mobile development\nRemote if possible",
+//     ),
+//   ];
+// }
