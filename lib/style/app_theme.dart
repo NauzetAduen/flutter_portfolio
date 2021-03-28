4d7b118241be09bce479901030f22f09e55131e1
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-28 22:51:43 +0100

    ADD texttheme to app

--- lib/style/app_theme.dart
+++ lib/style/app_theme.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
 const Color backGroundGreyColor = Color(0xff38383C);
+const Color lightGray = Color(0xffA5A5A5);
+const String ptSans = "PTsans";
+const String ptSerif = "PTserif";
+const TextTheme textTheme = TextTheme(
+  headline1: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontFamily: ptSerif,
+      fontSize: 144,
+      color: Colors.white,
+      height: 0.5),
+  headline2: TextStyle(
+      fontFamily: ptSans,
+      fontSize: 40,
+      letterSpacing: -2,
+      color: lightGray,
+      height: 0.6),
+);
 ThemeData themeData = ThemeData(
   scaffoldBackgroundColor: backGroundGreyColor,
+  textTheme: textTheme,
 );
