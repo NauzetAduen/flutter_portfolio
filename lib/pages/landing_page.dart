146f5e130ed8210238b78e04e9ee84b4a1d4fe49
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-28 22:52:01 +0100

    ADD LandingPage text and animations

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
-class LandingPage extends StatelessWidget {
+const animationDuration = Duration(milliseconds: 2500);
+
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  Animation<Offset> _titleAnimation;
+  Animation<Offset> _subtitleAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this, duration: animationDuration)
+      ..forward();
+
+    _titleAnimation = _getTweenAnimation(startPoint: -10);
+    _subtitleAnimation = _getTweenAnimation(startPoint: 10);
+  }
+
+  Animation<Offset> _getTweenAnimation({@required double startPoint}) {
+    return Tween<Offset>(
+      begin: Offset(startPoint, 0.0),
+      end: const Offset(0.0, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutQuint,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("maaaaaaa"),
-      ),
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SlideTransition(
+              position: _titleAnimation,
+              child: Text("nau", style: Theme.of(context).textTheme.headline1)),
+          SlideTransition(
+              position: _subtitleAnimation,
+              child: Text("flutter developer",
+                  style: Theme.of(context).textTheme.headline2))
+        ],
+      )),
     );
   }
 }
