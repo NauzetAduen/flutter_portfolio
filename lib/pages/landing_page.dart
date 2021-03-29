509a84c377fd5df34de6d5a3bad771a629658fb4
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-29 23:56:05 +0100

    ADD bar and linkcolumn animation, REFACTOR into method

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -13,6 +13,8 @@ class _LandingPageState extends State<LandingPage>
 
   Animation<Offset> _titleAnimation;
   Animation<Offset> _subtitleAnimation;
+  Animation<Offset> _barAnimation;
+  Animation<Offset> _linkColumnAnimation;
   @override
   void initState() {
     super.initState();
@@ -20,35 +22,84 @@ class _LandingPageState extends State<LandingPage>
     _controller = AnimationController(vsync: this, duration: animationDuration)
       ..forward();
 
-    _titleAnimation = _getTweenAnimation(startPoint: -10);
-    _subtitleAnimation = _getTweenAnimation(startPoint: 10);
+    // _controller.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     Future.delayed(const Duration(seconds: 2), () {
+    //       _controller.reverse();
+    //     });
+    //   }
+    // });
+
+    _titleAnimation = _getTweenAnimation(beginX: -10);
+    _subtitleAnimation = _getTweenAnimation(beginX: 10);
+    // _barAnimation = _getTweenAnimation(beginX: beginX, beginY: beginY, endX: endX, endY: endY, begin: begin, end: end)
+    _barAnimation = _getTweenAnimation(beginY: 200);
+    _linkColumnAnimation = _getTweenAnimation(beginY: -200);
   }
 
-  Animation<Offset> _getTweenAnimation({@required double startPoint}) {
+  Animation<Offset> _getTweenAnimation(
+      {double beginX = 0.0,
+      double beginY = 0.0,
+      double endX = 0.0,
+      double endY = 0.0,
+      double begin = 0,
+      double end = 1}) {
     return Tween<Offset>(
-      begin: Offset(startPoint, 0.0),
-      end: const Offset(0.0, 0.0),
+      begin: Offset(beginX, beginY),
+      end: Offset(endX, endY),
     ).animate(CurvedAnimation(
         parent: _controller,
-        curve: const Interval(0.5, 1, curve: Curves.easeOutQuint)));
+        curve: Interval(begin, end, curve: Curves.easeOutQuint)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
+      body: Stack(
         children: [
+          Center(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SlideTransition(
+                  position: _titleAnimation,
+                  child: Text("nau",
+                      style: Theme.of(context).textTheme.headline1)),
+              SlideTransition(
+                  position: _subtitleAnimation,
+                  child: Text("flutter developer",
+                      style: Theme.of(context).textTheme.headline2))
+            ],
+          )),
+          Positioned(
+            top: 30,
+            right: 30,
+            child: SlideTransition(
+                position: _barAnimation,
+                child: const Text(
+                  "me work",
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
           SlideTransition(
-              position: _titleAnimation,
-              child: Text("nau", style: Theme.of(context).textTheme.headline1)),
-          SlideTransition(
-              position: _subtitleAnimation,
-              child: Text("flutter developer",
-                  style: Theme.of(context).textTheme.headline2))
+            position: _linkColumnAnimation,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Container(
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [Text("A"), Text("B")],
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
-      )),
+      ),
     );
   }
 }
