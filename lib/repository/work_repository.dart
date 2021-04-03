b1a8ff4567a2463453bff1f41b121f0a832f82f1
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-04-03 22:09:39 +0100

    FIX bug with strings  code-coding

--- lib/repository/work_repository.dart
+++ lib/repository/work_repository.dart
@@ -4,14 +4,18 @@ class WorkRepository {
   static List<Work> getWorks() {
     return [
       Work(
-        title: "Coding Challenge",
+        title: "Code Challenge",
         date: "mar, 2021",
         description: """
-        This was an interesting challenge where I had to to create an application that allows users to: search for places in a location given by the user using the Foursquare API, view their search results and view the individual details of each search result.
- I use Clean Architecture by Uncle Bob adapted by ResoCoder, and I use package by feature as a code organisation.
- I used the 'TDD metodology' to test each layer of the app but I didn't implement any UI test.
- This app have a typeahead search functionality for search venues close to your location. After that shows a list of venues, which you can also see in a Google Map. You can check a detailed view of each venue with more information.
- You also have the ability to like a venue, using the shared_preferences package for it, so its stored in your phone.""",
+This was an interesting challenge where I had to to create an application that allows users to: search for places in a location given by the user using the Foursquare API, view their search results and view the individual details of each search result.
+
+I use Clean Architecture by Uncle Bob adapted by ResoCoder, and I use package by feature as a code organisation.
+
+I used the 'TDD metodology' to test each layer of the app but I didn't implement any UI test.
+
+This app have a typeahead search functionality for search venues close to your location. After that shows a list of venues, which you can also see in a Google Map. You can check a detailed view of each venue with more information.
+
+You also have the ability to like a venue, using the shared_preferences package for it, so its stored in your phone.""",
         chips: [
           "Flutter",
           "Mobile Dev",
@@ -21,17 +25,17 @@ class WorkRepository {
           "TTD"
         ],
         url: "https://github.com/NauzetAduen/coding-challenge",
-        imageURL: "coding_challenge.png",
+        imageURL: "code_challenge.png",
       ),
       Work(
         title: "Portfolio re-design",
         date: "mar, 2021",
         description: """
-        Redesign of my portfolio. Completely change the idea and theme. Learnt about hiearchy, colors and proportions.
+Redesign of my portfolio. Completely change the idea and theme. Learnt about hiearchy, colors and proportions.
 
-        Simplified the navigation through the portfolio, making animations instuitive for the visitor.
+Simplified the navigation through the portfolio, making animations instuitive for the visitor.
 
-        Adds better showcase images for each project and remove non important projects.
+Adds better showcase images for each project and remove non important projects.
         """,
         chips: [
           "Flutter Web",
@@ -44,13 +48,13 @@ class WorkRepository {
         title: "Diabetiary",
         date: "mar, 2020",
         description: """
-        Diabetiary, an app for diabetic patients. This app will simplify the user interface in order to help the user to understand how the illness is working, in order to fix it or keep it right.
+Diabetiary, an app for diabetic patients. This app will simplify the user interface in order to help the user to understand how the illness is working, in order to fix it or keep it right.
 
-        It's based on a tipical diabetic journal that you show to your doctor.
+It's based on a tipical diabetic journal that you show to your doctor.
 
-        Theme, fonts and general UI/UX design may change.
+Theme, fonts and general UI/UX design may change.
 
-        Work In Progress.
+Work In Progress.
         """,
         chips: [
           "Flutter",
@@ -65,15 +69,15 @@ class WorkRepository {
         title: "University Final Degree Project",
         date: "apr, 2020",
         description: """
-        After trying Flutter and Android native, I choose the first to be the framework in my final degree project. At first, I explored the possibility of a an app for deaf people, helping them in phonecalls, where the app would translate into text. I kind of make it work in a couple of days, so I tought the scope of the app was not big enough."
+After trying Flutter and Android native, I choose the first to be the framework in my final degree project. At first, I explored the possibility of a an app for deaf people, helping them in phonecalls, where the app would translate into text. I kind of make it work in a couple of days, so I tought the scope of the app was not big enough."
         
-        Finally I decided to develop a project called “Mobile app and backend to manage hiking trails”, where the user could create hiking trails, share, like and rate them. You could also create events in those trails. The full description can be found in the oficial document in the repository.
+Finally I decided to develop a project called “Mobile app and backend to manage hiking trails”, where the user could create hiking trails, share, like and rate them. You could also create events in those trails. The full description can be found in the oficial document in the repository.
         
-        The backend was Firebase, which helped me to create a completely reactive app.
+The backend was Firebase, which helped me to create a completely reactive app.
           
-          I Learned Flutter framework, the Dart Programming Language and the cloud firestore ecosystem.
+I Learned Flutter framework, the Dart Programming Language and the cloud firestore ecosystem.
           
-          I also used multiples technologies like Google+ Auth, Firestore Auth, Google Maps, Json serialization and NoSQL, Reactive Programming, etc.
+I also used multiples technologies like Google+ Auth, Firestore Auth, Google Maps, Json serialization and NoSQL, Reactive Programming, etc.
         """,
         chips: [
           "Flutter",
@@ -83,7 +87,7 @@ class WorkRepository {
           "Firebase",
         ],
         url: "",
-        imageURL: "diabetiary.png",
+        imageURL: "hiking_app.png",
       ),
     ];
   }
