9093ce286390568fcc2fdf36ac0b2e79023fca78
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-04-04 19:36:54 +0100

    ADD correct layout for workpage list

--- lib/pages/work_page.dart
+++ lib/pages/work_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/model/work.dart';
 import 'package:flutter_portfolio/repository/work_repository.dart';
+import 'package:flutter_portfolio/widgets/links_column.dart';
 import 'package:flutter_portfolio/widgets/navigation_bar.dart';
 
 class WorkPage extends StatefulWidget {
@@ -28,95 +29,122 @@ class _WorkPageState extends State<WorkPage> {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          ListView.builder(
-            itemBuilder: (context, index) {
-              return SizedBox(
-                  width: width,
-                  height: height,
-                  child: Center(
-                      child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Text(works[index].title,
-                            style: Theme.of(context).textTheme.bodyText2),
-                        Row(
+          SizedBox(
+            // width: width,
+            height: height * 0.8,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return SizedBox(
+                    width: width,
+                    // height: height * 0.8,
+                    child: Center(
+                        child: SingleChildScrollView(
+                      child: SizedBox(
+                        height: height * 0.8,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          // mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 32),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      works[index].description,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText1,
-                                      textAlign: TextAlign.justify,
-                                    ),
-                                    Row(
-                                      children: [
-                                        for (var chip in works[index].chips)
-                                          Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 2),
-                                            child: Chip(
-                                              // elevation: 2,
-                                              // shadowColor: Colors.grey[60],
-                                              shape: StadiumBorder(
-                                                  side: BorderSide(
-                                                      color: Theme.of(context)
-                                                          .scaffoldBackgroundColor)),
-
-                                              // padding:
-                                              //     EdgeInsets.symmetric(horizontal: ),
-                                              label: Text(
-                                                chip,
-                                                style: TextStyle(
-                                                    fontSize: 12,
-                                                    color: Theme.of(context)
-                                                        .primaryColor),
-                                              ),
-                                              backgroundColor:
-                                                  Theme.of(context).focusColor,
-                                            ),
-                                          ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 15),
+                              child: Text(works[index].title,
+                                  style: Theme.of(context).textTheme.subtitle1),
                             ),
-                            Expanded(
-                                child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 32),
-                              child: Image.asset(
-                                'images/${works[index].imageURL}',
-                              ),
-                            )),
+                            Text(
+                              works[index].date.toUpperCase(),
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 32),
+                                    child: Center(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        // mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            works[index].description,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyText1,
+                                            textAlign: TextAlign.justify,
+                                          ),
+                                          Wrap(
+                                            alignment: WrapAlignment.center,
+                                            // mainAxisAlignment:
+                                            //     MainAxisAlignment.center,
+                                            children: [
+                                              for (var chip
+                                                  in works[index].chips)
+                                                Chip(
+                                                  shape: StadiumBorder(
+                                                      side: BorderSide(
+                                                          color: Theme.of(
+                                                                  context)
+                                                              .scaffoldBackgroundColor)),
+
+                                                  // padding:
+                                                  //     EdgeInsets.symmetric(horizontal: ),
+                                                  label: Text(
+                                                    chip,
+                                                    style: TextStyle(
+                                                        fontSize: 12,
+                                                        color: Theme.of(context)
+                                                            .primaryColor),
+                                                  ),
+                                                  backgroundColor:
+                                                      Theme.of(context)
+                                                          .focusColor,
+                                                ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 32),
+                                      child: Material(
+                                        color: Colors.transparent,
+                                        elevation: 20,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(12.0),
+                                          child: Image.asset(
+                                            'images/${works[index].imageURL}',
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            works[index].date,
-                            style: Theme.of(context).textTheme.bodyText2,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )));
-            },
-            itemCount: works.length,
-            // physics: const NeverScrollableScrollPhysics(),
-            scrollDirection: Axis.horizontal,
+                      ),
+                    )));
+              },
+              itemCount: works.length,
+              // physics: const NeverScrollableScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+            ),
           ),
           Positioned(
             top: 30,
             right: 30,
             child: NavigationBar(),
           ),
+          LinksColumn(),
         ],
       ),
       // ),
