a8f664d774974c8d7169222aa008e3813ae0232a
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-04-05 01:20:38 +0100

    ADD paintLayout in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_strategy/url_strategy.dart';
+// import 'package:flutter/rendering.dart';
 
 import 'theme/app_theme.dart';
 import 'utils/custom_router.dart';
@@ -11,11 +12,14 @@ void main() {
 
 class Portfolio extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'nau',
-        theme: themeData,
-        initialRoute: "/",
-        onGenerateRoute: CustomRouter.generateRoute,
-      );
+  Widget build(BuildContext context) {
+    // debugPaintSizeEnabled = true;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'nau',
+      theme: themeData,
+      initialRoute: "/",
+      onGenerateRoute: CustomRouter.generateRoute,
+    );
+  }
 }
