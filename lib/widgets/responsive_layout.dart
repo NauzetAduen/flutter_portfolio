79ab0b9c78683d95ccb636c158f528aace8eeb7d
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-04-05 21:30:43 +0100

    ADD base responsive layout widget

---
+++ lib/widgets/responsive_layout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/utils/constrains.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobile;
+  final Widget desktop;
+
+  const ResponsiveLayout({
+    @required this.mobile,
+    @required this.desktop,
+  });
+  @override
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (context, constrains) =>
+            (constrains.maxWidth <= kMobileWidth) ? mobile : desktop,
+      );
+}
