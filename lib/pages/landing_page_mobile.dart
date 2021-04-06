34d34b28e942c71e97d8969d89e3cec434996ed8
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-04-06 01:14:31 +0100

    ADD landing page mobile

---
+++ lib/pages/landing_page_mobile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/widgets/mobile_app_bar.dart';
+
+class LandingPageMobile extends StatelessWidget {
+  const LandingPageMobile();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(80.0),
+          child: MobileAppBar(),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("nau", style: Theme.of(context).textTheme.headline1),
+              RichText(
+                text: TextSpan(children: <TextSpan>[
+                  TextSpan(
+                      text: 'flutter',
+                      style: Theme.of(context).textTheme.headline2),
+                  TextSpan(
+                      text: 'dev',
+                      style: Theme.of(context).textTheme.headline3),
+                  TextSpan(
+                      text: 'eloper',
+                      style: Theme.of(context).textTheme.headline2),
+                ]),
+              ),
+            ],
+          ),
+        ));
+  }
+}
