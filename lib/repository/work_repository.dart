ea74bcd4a0d8e4a1d23fa4658a62c9bf609471ae
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-04-06 16:02:37 +0100

    Simplify work repository descriptions

--- lib/repository/work_repository.dart
+++ lib/repository/work_repository.dart
@@ -7,15 +7,13 @@ class WorkRepository {
         title: "Code Challenge",
         date: "mar, 2021",
         description: """
-This was an interesting challenge where I had to to create an application that allows users to: search for places in a location given by the user using the Foursquare API, view their search results and view the individual details of each search result.
+Application which allows users to search for places using the Foursquare API, view their search results and view the individual details of each search result.
 
 I use Clean Architecture by Uncle Bob adapted by ResoCoder, and I use package by feature as a code organisation.
 
 I used the 'TDD metodology' to test each layer of the app but I didn't implement any UI test.
 
-This app have a typeahead search functionality for search venues close to your location. After that shows a list of venues, which you can also see in a Google Map. You can check a detailed view of each venue with more information.
-
-You also have the ability to like a venue, using the shared_preferences package for it, so its stored in your phone.""",
+You have a typeahead pattern, uses geolocation, allows you to like venues, shows a GoogleMap view""",
         chips: [
           "Flutter",
           "Mobile Dev",
@@ -69,15 +67,12 @@ Work In Progress.
         title: "University Final Degree Project",
         date: "apr, 2020",
         description: """
-After trying Flutter and Android native, I choose the first to be the framework in my final degree project. At first, I explored the possibility of a an app for deaf people, helping them in phonecalls, where the app would translate into text. I kind of make it work in a couple of days, so I tought the scope of the app was not big enough."
-        
-Finally I decided to develop a project called “Mobile app and backend to manage hiking trails”, where the user could create hiking trails, share, like and rate them. You could also create events in those trails. The full description can be found in the oficial document in the repository.
+
+In this project users can create hiking trails and events, share, like and rate them.
         
 The backend was Firebase, which helped me to create a completely reactive app.
           
 I Learned Flutter framework, the Dart Programming Language and the cloud firestore ecosystem.
-          
-I also used multiples technologies like Google+ Auth, Firestore Auth, Google Maps, Json serialization and NoSQL, Reactive Programming, etc.
         """,
         chips: [
           "Flutter",
